17f73e251811053f0f63e0653fab6663f27189c5
zulfidly <zulfidly@gmail.com>
2023-06-18 17:50:44 +0800

    1

--- server/api/serverClock.get.js
+++ server/api/serverClock.get.js
@@ -3,15 +3,6 @@ export default defineEventHandler(async(event) => {
     let serverDateNow = Date.now()
     let offsetTimeinMins = serverUTC_full.getTimezoneOffset()
 
-    let serverLocal = new Intl.DateTimeFormat(
-        'en-GB',
-        { dateStyle: 'full', hour12:true, timeStyle: 'long' }
-    ).format(new Date(serverDateNow + offsetms))
-
-    // let ts = serverUTC_full.getFullYear().toString() + '-' + (serverUTC_full.getMonth()+1).toString().padStart(2,0) + '-' + serverUTC_full.getDate().toString().padStart(2,0)
-    // let c = serverUTC_full.getHours().toString().padStart(2,0) + ':' + serverUTC_full.getMinutes().toString().padStart(2,0) 
-    // console.log('timestamp :', ts);
-    // console.log('server Clock =', c);       // getHours() returns (0-23)
     let obj = {
         "serverUTC": serverUTC_full,
         "serverUTC_ms": serverDateNow,
