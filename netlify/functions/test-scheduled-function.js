0475e997b237ca54a3d4c13d6f2195a00ccb8a5a
zulfidly <zulfidly@gmail.com>
2023-06-19 19:49:16 +0800

    3

--- netlify/functions/test-scheduled-function.js
+++ netlify/functions/test-scheduled-function.js
@@ -1,6 +1,7 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/test-scheduled-function.js
 import { getRawData } from "../../server/api/scraper.get.js";
-const { schedule } = require("@netlify/functions");
+import { schedule } from "@netlify/functions"
+// const { schedule } = require("@netlify/functions");
 
 const handler = async function(event, context) {
     console.log("Received event:", event, 'Context:', context);
@@ -10,5 +11,5 @@ const handler = async function(event, context) {
     };
 };
 
-// exports.handler = schedule("@hourly", handler);
+// exports.handler = schedule("@hourly", handler);          // “At minute 0 every hour” https://crontab.guru/
 exports.handler = schedule("1/10 * * * *", handler);        //“At every 10th minute from 1 through 59.”
