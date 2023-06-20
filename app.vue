a27558b8cf9397c3986e7327eac9c9600a3a3298
zulfidly <zulfidly@gmail.com>
2023-06-21 06:28:21 +0800

    8

--- app.vue
+++ app.vue
@@ -1,30 +1,29 @@
 <template>  
-  <div class="w-full h-full flex flex-col justify-center items-center gap-4">
-    <header class="animate-pulse text-xl text-center">coding practice : experimental web scraper
-      
-    </header>
 
-    <div class="relative p-4 w-auto max-h-[90svh] border-2 rounded-xl overflow-scroll">
-      <button class="z-10 absolute mt-1 mr-1 top-0 right-0 bg-[var(--color-border)] py-0.5 px-3 rounded-lg">
-        <a href="https://airtable.com/shrmTGaN1fcaE0kRA" target="_blank">view Airtable</a>
-      </button>
-      <table class="relative min-w-[80vw]">
-        <caption class="mt-3 text-left text-lg font-bold">Daily statistics for Inari Amertron Bhd [0166]</caption>
+  <div class=" w-full h-full flex flex-col justify-start items-center gap-4">
+
+    <div class="flex flex-col px-2 py-6 lg:p-4 w-auto max-h-[90svh] border rounded-xl overflow-scroll bg-[var(--color-background-soft)] border-[var(--color-border)]">
+      <table class="min-w-[80vw] max-w-[90vw]  bg-[var(--color-background-mute)]">
+        <caption class="mb-2 text-center text-md lg:text-lg font-normal lg:font-bold ">
+          Daily statistics for <a class="underline decoration-dotted" href="https://www.malaysiastock.biz/Corporate-Infomation.aspx?securityCode=0166" target="_blank" aria-label="visit Inari website">Inari Amertron Bhd</a> [0166] 
+          <br> <span class="text-sm font-light italic">- this is a web scraping coding practice -</span>
+        </caption>
         <tbody>
-          <tr class="[&>th]:text-md [&>th]:font-bold [&>th]:text-center [&>th]:border">
+          <tr class="[&>th]:p-1 [&>th]:lg:p-2 [&>th]:text-md [&>th]:font-light [&>th]:lg:font-bold [&>th]:text-center [&>th]:border [&>th]:border-[var(--color-border)]">
             <th>Date</th>
-            <th>Day</th>
-            <th>Time Read</th>
+            <th class="hidden lg:table-cell">Day</th>
+            <th class="hidden lg:table-cell">Time Read</th>
             <th>Previous Close</th>
             <th>Open</th>
             <th>Day's Range</th>
             <th>Last Done</th>
           </tr>
 
-          <tr v-for="(x, ind) in entries" class="[&>td]:p-1 [&>td]:text-center [&>td]:border">
-            <td>{{ x.fields.Date }}</td>
-            <td>{{ x.fields.Day }}</td>
-            <td>{{ x.fields.Time24h }}</td>
+          <tr v-for="(x, ind) in entries" :key="'row'+ind" class="[&>td]:even:bg-[var(--color-background-soft)] [&>td]:p-0.5 [&>td]:lg:p-2 [&>td]:text-center [&>td]:font-light [&>td]:lg:font-medium [&>td]:border [&>td]:border-[var(--color-border)]">
+            <td class="lg:hidden">{{ new Intl.DateTimeFormat('en-GB').format(new Date(x.fields.Date)).slice(0, 5) }}</td>
+            <td class="hidden lg:table-cell">{{ new Intl.DateTimeFormat('en-GB').format(new Date(x.fields.Date)) }}</td>
+            <td class="hidden lg:table-cell">{{ x.fields.Day }}</td>
+            <td class="hidden lg:table-cell">{{ x.fields.Time24h }}</td>
             <td>{{ x.fields.PreviousClose }}</td>
             <td>{{ x.fields.Open }}</td>
             <td>{{ x.fields.DayRange }}</td>
@@ -32,6 +31,21 @@
           </tr>
         </tbody>  
       </table>
+
+      <section class="place-self-end flex mt-12 gap-6">
+          <a class="px-3 m-auto border border-[var(--color-border)] rounded-lg bg-[var(--color-background-mute)]" href="https://portfolio-fidly.netlify.app/" target="_blank" aria-label="visit portfolio">
+            coded by fidly
+          </a>
+
+        <IconGitHub />
+        <button class=" mr-2 border border-[var(--color-border)] bg-[var(--color-background-mute)] rounded-lg">
+          <a class="text-sm italic font-extralight" href="https://airtable.com/shrmTGaN1fcaE0kRA" target="_blank">
+            view raw data
+            <IconAirtable class="m-auto p-0.5"/>
+          </a>
+        </button>
+
+      </section>
 
     </div>
   </div>
