34dcd5fa00b3ccfd48329245fae6268581714005
zulfidly <zulfidly@gmail.com>
2023-06-23 10:46:51 +0800

    10

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Note that scheduled functions don’t work with payloads or POST request data. W
 
 
 npm i @vueuse/core
+
+exports.handler = schedule("30 5,10 * * 1-5", handler);   
+// Standard cron UTC: “At minute 30 past hour 5 and 10 on every day-of-week from Monday through Friday.”   https://crontab.guru/
+//translates to 1:30pm & 6:30pm GMT +8, Mon-Fri
--- app.vue
+++ app.vue
@@ -16,7 +16,7 @@
 
   onMounted(()=> {
     console.log('app.vue mounted');
-    console.log('clock', clock_ph.data._rawValue.clock);
+    // console.log('clock', clock_ph.data._rawValue.clock);
     // console.log('PH', clock_ph.data._rawValue.publicHols);
     clockServerRef.value = clock_ph.data._rawValue.clock
     clockCalibratedForClient.value = clock_ph.data._rawValue.clock.calibrated
@@ -36,7 +36,7 @@
 
 <template>  
   <div :style="getHeight" class="relative w-full flex flex-col justify-evenly items-center border-0 overflow-clip lg:border lg:rounded-xl lg:shadow-md transition-all duration-700">
-    <ColorModeSwitch @click="toggleDark()" />
+    <ColorModeSwitch @click="toggleDark()" class="md:hidden lg:block"/>
     <p class="sm:hidden lg:block p-3 fixed lg:absolute left-0 bottom-0 text-sm italic font-thin">* figures shown in MYR</p>
       <p class="sm:hidden lg:block mb-4 text-center text-md lg:text-lg font-normal lg:font-bold tracking-wide">
         Daily statistics for
@@ -54,7 +54,7 @@
             <tr class="[&>*]:mx-auto [&>*]:lg:p-2 [&>*]:text-md [&>*]:font-light [&>*]:lg:font-bold [&>*]:text-center">
               <th>Date</th>
               <th class="hidden lg:table-cell">Day</th>
-              <th class="hidden lg:table-cell">Time Read</th>
+              <th class="hidden md:table-cell">Time<br>Read</th>
               <th class="text-justify">Previous<br>Close </th>
               <th>Open</th>
               <th>Day's <br>Range</th>
@@ -68,7 +68,7 @@
               <td class="lg:hidden">{{ new Intl.DateTimeFormat('en-GB').format(new Date(x.fields.Date)).slice(0, 5) }}</td>
               <td class="hidden lg:table-cell">{{ new Intl.DateTimeFormat('en-GB').format(new Date(x.fields.Date)) }}</td>
               <td class="hidden lg:table-cell">{{ x.fields.Day }}</td>
-              <td class="hidden lg:table-cell">{{ x.fields.Time24h }}</td>
+              <td class="hidden md:table-cell">{{ x.fields.Time24h }}</td>
               <td>{{ x.fields.PreviousClose }}</td>
               <td>{{ x.fields.Open }}</td>
               <td>{{ x.fields.DayRange }}</td>
@@ -78,7 +78,7 @@
         </table>  
       </div>
 
-      <section class="sm:hidden lg:block place-self-end mt-4 lg:mr-6">
+      <section class="md:hidden lg:block place-self-end mt-4 lg:mr-6">
         <div class="flex gap-4 lg:gap-12">
           <a class="px-3 m-auto font-light border border-[var(--color-border)] rounded-lg bg-[var(--color-background-mute)]" href="https://portfolio-fidly.netlify.app/" target="_blank" aria-label="visit portfolio">
             coded by fidly
