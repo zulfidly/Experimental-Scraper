f1ce0cee08614b7bb6975ce27972ae2edd349ce7
zulfidly <zulfidly@gmail.com>
2023-06-24 23:48:07 +0800

    12

--- netlify/functions/dummy1-background.js
+++ netlify/functions/dummy1-background.js
@@ -1,6 +1,6 @@
 // import { schedule } from "@netlify/functions"
 
-const handler = async function(event, context) {
+exports.handler = function(event, context) {
     return { statusCode: 200, body:'API call successful' };
 };
 // exports.handler = schedule("1/5 * * * *", handler);   
